48350d367cd989862c58ed78852043300c61b64e
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-08-27 18:48:18 -0700

    dotenv

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express') // web server framework
 const app = express() // create the web server
 const fs = require('fs') // filesystem library
@@ -32,6 +34,6 @@ app.get('/reset', (req,res) => {
 })
 
 app.use(express.static(__dirname)) // serve all files in the current folder so we can access index.html
-app.listen(80) // start server on port 80
+app.listen(process.env.PORT) // start server on port 80
 
 console.log('Server started on port 80.')
